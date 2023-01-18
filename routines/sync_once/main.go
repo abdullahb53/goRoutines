d108d7409cba10d4c98cd71a1ad67657ea963c65
Abdullah BIYIK <abdullahb5355@gmail.com>
2023-01-18 19:25:23 +0300

    sync_once added

---
+++ routines/sync_once/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var do sync.Once
+var Number int
+
+func init() {
+	Number = 0
+}
+
+func JustDoIt(s1 int) {
+	Number += s1
+}
+
+func main() {
+
+	// We are guaranteed to work once in the 'Just Do It' function.
+	do.Do(func() { JustDoIt(53) })
+	fmt.Println("Number:", Number)
+
+	do.Do(func() { JustDoIt(22) })
+	fmt.Println("Number:", Number)
+
+	do.Do(func() { JustDoIt(11) })
+	fmt.Println("Number:", Number)
+
+}
