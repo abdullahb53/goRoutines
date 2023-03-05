a124c8c8c64f76869382f2d5c1e1ec3582e43991
Abdullah BIYIK <abdullahb5355@gmail.com>
2023-03-06 01:26:14 +0300

    rangeover

---
+++ rangeover_channel/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func SendDataToChannel(channel chan<- string, data string) {
+	channel <- data
+
+}
+
+func main() {
+
+	var queue = make(chan string, 5)
+
+	SendDataToChannel(queue, "1")
+	SendDataToChannel(queue, "2")
+	SendDataToChannel(queue, "3")
+	SendDataToChannel(queue, "4")
+	// SendDataToChannel(queue, "5")
+
+	close(queue)
+
+	for channel_data := range queue {
+		fmt.Println("Element:", channel_data)
+	}
+
+}
