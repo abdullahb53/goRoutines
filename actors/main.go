19fbe411efd9e0dd5b6a7e1b7b3d39cbdd61d432
Abdullah BIYIK <abdullahb5355@gmail.com>
2023-03-10 01:40:38 +0300

    actor-concurrency-model

---
+++ actors/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/anthdm/hollywood/actor"
+)
+
+type session struct {
+	conn net.Conn
+}
+
+type connAdd struct {
+	pid  *actor.PID
+	conn net.Conn
+}
+
+func newSession(conn net.Conn) actor.Producer {
+	return func() actor.Receiver {
+		return &session{
+			conn: conn,
+		}
+	}
+
+}
+
+func (s *session) Receive(c *actor.Context) {
+	switch msg := c.Message().(type) {
+	case actor.Initialized:
+
+	case actor.Started:
+		log.Println("new connection:", s.conn.RemoteAddr())
+		go s.readLoop(c)
+	case actor.Stopped:
+	case []byte:
+		s.conn.Write(msg)
+	}
+}
+
+func (s *session) readLoop(c *actor.Context) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := s.conn.Read(buf)
+		if err != nil {
+			log.Println("conn read error", err)
+			break
+		}
+		msg := buf[:n]
+		c.Send(c.PID(), msg)
+	}
+}
+
+type server struct {
+	listenAddr string
+	ln         net.Listener
+	sessions   map[*actor.PID]net.Conn
+}
+
+func newServer(listenAddr string) actor.Producer {
+	return func() actor.Receiver {
+		return &server{
+			listenAddr: listenAddr,
+			sessions:   make(map[*actor.PID]net.Conn),
+		}
+	}
+}
+
+func (s *server) Receive(c *actor.Context) {
+	switch msg := c.Message().(type) {
+	case actor.Initialized:
+		ln, err := net.Listen("tcp", s.listenAddr)
+		if err != nil {
+			panic(err)
+		}
+		s.ln = ln
+	case actor.Started:
+		log.Println("server started", s.listenAddr)
+		go s.acceptLoop(c)
+	case actor.Stopped:
+		_ = msg
+	case *connAdd:
+		log.Println("added new conn to my map", msg.conn.RemoteAddr())
+		s.sessions[msg.pid] = msg.conn
+	}
+}
+
+func (s *server) acceptLoop(c *actor.Context) {
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			log.Println("accept error", err)
+			break
+		}
+		pid := c.SpawnChild(newSession(conn), "session", actor.WithTags(conn.RemoteAddr().String()))
+		c.Send(c.PID(), &connAdd{
+			pid:  pid,
+			conn: conn,
+		})
+	}
+}
+
+func main() {
+	e := actor.NewEngine()
+	e.Spawn(newServer(":6000"), "server")
+
+	<-make(chan struct{})
+
+}
--- go.mod
+++ go.mod
@@ -3,11 +3,16 @@ module goRoutines
 go 1.19
 
 require (
+	github.com/MauriceGit/skiplist v0.0.0-20211105230623-77f5c8d3e145
+	github.com/anthdm/hollywood v0.0.0-20230306194734-bae4cfa6e85d
 	github.com/gorilla/websocket v1.5.0
 	github.com/rabbitmq/amqp091-go v1.7.0
 )
 
 require (
-	github.com/MauriceGit/skiplist v0.0.0-20211105230623-77f5c8d3e145 // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/zeebo/xxh3 v1.0.2 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
