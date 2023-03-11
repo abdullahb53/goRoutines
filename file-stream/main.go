9745690059decc62f79d528fbe0f74babc82285b
Abdullah BIYIK <abdullahb5355@gmail.com>
2023-03-11 18:30:47 +0300

    worker-pool

--- file-stream/main.go
+++ file-stream/main.go
@@ -41,7 +41,7 @@ func (fs *FileServer) readLoop(conn_sock net.Conn) {
 			log.Fatalf("connection_socket err: %x", err)
 		}
 
-		// fmt.Println(buf.Bytes()) // show data from buffer
+		fmt.Println(buf.Bytes()) // show data from buffer
 		fmt.Println("Received data over tcp,size:", i)
 	}
 
@@ -74,7 +74,7 @@ func main() {
 
 	go func() {
 		time.Sleep(4 * time.Second)
-		err := senderSimulation(3000)
+		err := senderSimulation(200000)
 		if err != nil {
 			log.Println("sender", err)
 		}
@@ -84,78 +84,3 @@ func main() {
 	newServer.start()
 
 }
-
-// package main
-
-// import (
-// 	"crypto/rand"
-// 	"fmt"
-// 	"io"
-// 	"log"
-// 	"net"
-// 	"time"
-// )
-
-// type FileServer struct {
-// }
-
-// func (fs *FileServer) start() {
-// 	ln, err := net.Listen("tcp", ":3000")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	for {
-// 		conn, err := ln.Accept()
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-
-// 		go fs.readLoop(conn)
-// 	}
-// }
-
-// func (fs *FileServer) readLoop(conn net.Conn) {
-// 	buf := make([]byte, 2048)
-// 	for {
-// 		n, err := conn.Read(buf)
-
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-// 		file := buf[:n]
-// 		fmt.Println(file)
-// 		fmt.Printf("received %d bytes over the network \n", n)
-
-// 	}
-
-// }
-// func sendFile(size int) error {
-// 	file := make([]byte, size)
-// 	_, err := io.ReadFull(rand.Reader, file)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	conn, err := net.Dial("tcp4", ":3000")
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	n, err := conn.Write(file)
-// 	if err != niListen()
-// 		return err
-// 	}
-// 	fmt.Printf("written %d  bytes over the network\n", n)
-// 	return nil
-// }
-
-// func main() {
-// 	go func() {
-// 		time.Sleep(4. * time.Second)
-// 		sendFile(1000)
-// 	}()
-// 	server := &FileServer{}
-// 	server.start()
-
-// }
--- worker-pool/main.go
+++ worker-pool/main.go
@@ -8,33 +8,46 @@ import (
 func worker(id int, jobs <-chan int, results chan<- int) {
 	// When our worker is created by main function, we be able to collect our jobs.
 	for i := range jobs {
-		fmt.Println("worker:", id, "Started job!:", i)
-		time.Sleep(time.Second)
-		fmt.Println("worker:", id, "Finished job..:", i)
-		results <- i * 3
+		// fmt.Println("worker:", id, "Finished job..:", i)
+		results <- i
 	}
 }
 
 func main() {
-	const numberJobs = 5
-	jobs := make(chan int, numberJobs)
-	results := make(chan int, numberJobs)
+	const Workers = 8
+	const _JOBS = 10000000
+	jobs := make(chan int, _JOBS)
+	results := make(chan int, _JOBS)
 
 	// Creating our workers that these are 5
-	// 'const numberJobs = 5'
-	for w := 1; w <= 3; w++ {
+	for w := 1; w <= Workers; w++ {
 		go worker(w, jobs, results)
 	}
 
-	// Sending jobs to 'jobs channel'.
-	for j := 1; j <= numberJobs; j++ {
-		jobs <- j + 3312
-	}
+	// Start timer after routines loaded.
+	start := time.Now()
 
-	close(jobs)
+	// Sending jobs to 'jobs channel'.
+	go func() {
+		for j := 1; j <= _JOBS; j++ {
+			jobs <- j + 331
+		}
+	}()
 
 	// We are collecting our finished jobs with 'results channel'.
-	for a := 1; a <= numberJobs; a++ {
-		<-results
+
+	i := 0
+break_there:
+	for {
+		select {
+		case <-results:
+			i++
+
+			if i >= _JOBS {
+				break break_there
+			}
+		}
 	}
+	elapsed := time.Since(start)
+	fmt.Printf("Time elapsed: %v\n", elapsed)
 }
