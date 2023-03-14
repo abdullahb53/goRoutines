63dffa7cebb8df4471473a8c45eb857ab13bb747
Abdullah BIYIK <abdullahb5355@gmail.com>
2023-03-14 23:47:08 +0300

    microservices

---
+++ microservice-structre/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	svc := NewCatFactService("https://catfact.ninja/fact")
+	svc = NewLoggingService(svc)
+
+	ApiServer := NewApiServer(svc)
+
+	log.Fatal(ApiServer.Start(":3000"))
+}
