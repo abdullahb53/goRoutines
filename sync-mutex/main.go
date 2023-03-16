f4145132576703ee4221d0dc5fe50eb3e1a7d0ed
Abdullah BIYIK <abdullahb5355@gmail.com>
2023-03-17 00:46:05 +0300

    sync-mutex

--- sync-mutex/main.go
+++ sync-mutex/main.go
@@ -2,33 +2,60 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
-var GlobalTotal = 0
+type Player struct {
+	mu     sync.RWMutex
+	health int32
+}
 
-const NUMBER = 100
+func (p *Player) takeDamage(value int) {
+	health := p.getHealth()
+	atomic.StoreInt32(&p.health, int32(health-value))
+	// p.mu.TryLock()
+	// p.health -= int32(value)
+	// p.mu.Unlock()
 
-func Add(wg *sync.WaitGroup, mutex *sync.Mutex, num int) {
+}
 
-	mutex.Lock()
-	GlobalTotal = GlobalTotal + num
-	mutex.Unlock()
-	wg.Done()
+func (p *Player) getHealth() int {
+	return int(atomic.LoadInt32(&p.health))
+}
+
+func NewPlayer() *Player {
+	return &Player{
+		health: 300,
+	}
+}
+
+func startUILoop(p *Player) {
+	ticker := time.NewTicker(time.Second)
+	for {
+		fmt.Println("Player health:", p.getHealth())
+		<-ticker.C
+	}
+}
+
+func startGameLoop(p *Player) {
+	ticker := time.NewTicker(time.Millisecond * 300)
+	for {
+		p.takeDamage(rand.Intn(40))
+		if p.getHealth() <= 0 {
+			fmt.Println("GAME OVER")
+			break
+		}
+		<-ticker.C
+	}
 }
 
 func main() {
-	mutex := new(sync.Mutex)
-	wg := new(sync.WaitGroup)
 
-	wg.Add(NUMBER)
-
-	for i := 0; i < 100; i++ {
-		go Add(wg, mutex, i)
-	}
-
-	wg.Wait()
-
-	fmt.Println("GlobalTotal:", GlobalTotal)
+	player := NewPlayer()
+	go startUILoop(player)
+	startGameLoop(player)
 
 }
