bc18bc23a5716b813d4f541aa8b839bc0d8c95f1
Abdullah BIYIK <abdullahb5355@gmail.com>
2023-08-17 00:10:42 +0300

    schedule

--- worker-pool/main.go
+++ worker-pool/main.go
@@ -1,57 +1,38 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+const WORKERsize = 256
 
-const (
-	workers = 8
-	jobs    = 10000000
-)
+type Pool struct {
+	Work chan func()
+	Cls  chan struct{}
+}
 
-func worker(id int, jobsChannel <-chan int, resultsChannel chan<- int) {
-	// When our worker is created by main function, we be able to collect our jobs.
-	for i := range jobsChannel {
-		// fmt.Println("worker:", id, "Finished job..:", i)
-		resultsChannel <- i
+func NewPool(size int) *Pool {
+	return &Pool{
+		Work: make(chan func()),
+		Cls:  make(chan struct{}, size),
+	}
+}
+
+func (p *Pool) worker(task func()) {
+	defer func() {
+		<-p.Cls
+	}()
+	for {
+		task()
+		task = <-p.Work
+	}
+}
+
+func (p *Pool) Schedule(task func()) {
+	select {
+	case p.Work <- task:
+	case p.Cls <- struct{}{}:
+		go p.worker(task)
 	}
 }
 
 func main() {
+	_ = NewPool(WORKERsize)
 
-	jobsChannel := make(chan int, jobs)
-	resultsChannel := make(chan int, jobs)
-
-	// Creating our workers that these are 5
-	for w := 1; w <= workers; w++ {
-		go worker(w, jobsChannel, resultsChannel)
-	}
-
-	// Start timer after routines loaded.
-	start := time.Now()
-
-	// Sending jobs to 'jobs channel'.
-	go func() {
-		for j := 1; j <= jobs; j++ {
-			jobsChannel <- j + 331
-		}
-	}()
-
-	// We are collecting our finished jobs with 'results channel'.
-
-	i := 0
-break_there:
-	for {
-		select {
-		case <-resultsChannel:
-			i++
-
-			if i >= jobs {
-				break break_there
-			}
-		}
-	}
-	elapsed := time.Since(start)
-	fmt.Printf("Time elapsed: %v\n", elapsed)
 }
